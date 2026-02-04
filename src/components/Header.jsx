bdfde7056245cc8f6b4130298b8e5c5ba4986471
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-04 18:22:18 -0300

    conteudo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ type Props = {}
 const Header = (props: Props) => {
   return (
     <div className='header'>
-      <h1>Trablahdno em equipe</h1>
+      <h1>Trabalhando em equipe</h1>
 
     </div>
   )
